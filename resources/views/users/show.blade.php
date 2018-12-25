3ef76808c4d6ac0cdbb1300a938d4b8113c09b98
vagrant <vagrant@homestead>
2018-12-25 16:31:52 +0000

    用户话题列表

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -24,10 +24,15 @@
         <h1 class="mb-0" style="font-size:22px;">{{ $user->name }} <small>{{ $user->email }}</small></h1>
       </div>
     </div>
-    <hr> {{-- 用户发布的内容 --}}
+    <hr>
+    {{-- 用户发布的内容 --}}
     <div class="card ">
       <div class="card-body">
-        暂无数据 ~_~
+        <ul class="nav nav-tabs">
+          <li class="nav-item"><a class="nav-link active bg-transparent" href="#">Ta 的话题</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Ta 的回复</a></li>
+        </ul>
+        @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
       </div>
     </div>
 
